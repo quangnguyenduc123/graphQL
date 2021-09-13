8ce92e751d8fa9d58ebd0ff892af2e26fc17092c
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-13 20:39:08 +0700

    set up simple-project
    - use concurrently to run multiple commands
    - config tsconfig.json

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiple(a, b) {
+    return a + b;
+}
+var a = 2;
+var b = 6;
+console.log(multiple(a, b));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function multiple(a: number, b: number): number {
+    return a + b
+}
+
+const a = 2
+const b = 6
+console.log(multiple(a, b))
