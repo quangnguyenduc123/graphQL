4837d1b56a76a717b513dc8f781cb69ff8fdf8b8
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-13 21:10:42 +0700

    module section

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-function multiple(a: number, b: number): number {
-    return a + b
-}
+import multiply, { multiplyBy2 } from './multiply'
+import { multiplyBy2 as mBy2 } from './multiply'
+import * as multiplyModule from './multiply'
 
 const a = 2
 const b = 6
-console.log(multiple(a, b))
+console.log(multiply(a, b))
+
+console.log(multiplyModule.default(a, b))
+console.log(multiplyModule.multiplyBy2(a))
