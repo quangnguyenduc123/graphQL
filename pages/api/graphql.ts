f502e2b8127390744fd935899fe90269694d1317
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-16 20:41:19 +0700

    codegen graphql

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
-import { ApolloServer, gql, IResolvers } from 'apollo-server-micro';
+import { ApolloServer, gql } from 'apollo-server-micro';
 import mysql from 'serverless-mysql';
-import { OkPacket } from 'mysql'
+import { OkPacket } from 'mysql';
+import { Resolvers, TaskStatus } from '../../generated/graphql-backend';
 
 const typeDefs = gql`
   enum TaskStatus {
@@ -40,33 +41,17 @@ interface ApolloContext {
   db: mysql.ServerlessMysql;
 }
 
-enum TaskStatus {
-  active = 'active',
-  completed = 'completed'
-}
-
-interface Task {
-  id: number
-  title: String
-  status: TaskStatus
-}
-
-
 interface TaskDbRow {
-  id: number
-  title: String
-  task_status: TaskStatus
+  id: number;
+  title: string;
+  task_status: TaskStatus;
 }
 
-type TasksDbQueryResult = TaskDbRow[]
+type TasksDbQueryResult = TaskDbRow[];
 
-const resolvers: IResolvers<any, ApolloContext> = {
+const resolvers: Resolvers<ApolloContext> = {
   Query: {
-    async tasks(
-      parent,
-      args: { status?: TaskStatus },
-      context
-    ): Promise<Task[]> {
+    async tasks(parent, args, context) {
       const { status } = args;
       let query = 'SELECT id, title, task_status FROM tasks';
       const queryParams: string[] = [];
@@ -74,7 +59,7 @@ const resolvers: IResolvers<any, ApolloContext> = {
         query += ' WHERE task_status = ?';
         queryParams.push(status);
       }
-      const tasks = await context.db.query < TasksDbQueryResult > (
+      const tasks = await context.db.query<TasksDbQueryResult>(
         query,
         queryParams
       );
@@ -90,19 +75,15 @@ const resolvers: IResolvers<any, ApolloContext> = {
     },
   },
   Mutation: {
-    async createTask(
-      parent,
-      args: { input: { title: string } },
-      context
-    ): Promise<Task> {
-      const result = await context.db.query < OkPacket > (
+    async createTask(parent, args, context) {
+      const result = await context.db.query<OkPacket>(
         'INSERT INTO tasks (title, task_status) VALUES(?, ?)',
-        [args.input.title, TaskStatus.active]
+        [args.input.title, TaskStatus.Active]
       );
       return {
         id: result.insertId,
         title: args.input.title,
-        status: TaskStatus.active,
+        status: TaskStatus.Active,
       };
     },
     updateTask(parent, args, context) {
