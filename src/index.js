365dd4fd99aef82c7bebe8bf4f33637ba8cb2962
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-19 22:31:07 +0700

    query with array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+const users = [{ id: '1', name: 'quang', age: '25' }, { id: '2', name: 'dai', age: '25' }]
 // Type defnitions(schema)
 const typeDefs = `
     type Query {
@@ -7,6 +8,7 @@ const typeDefs = `
         user: User!
         post: Post!
         add(a: Int!, b: Int!): Int!
+        users(query: String): [User!]!
     }
 
     type User {
@@ -40,6 +42,14 @@ const resolvers = {
         },
         post() {
             return { id: '1', title: 'post1', body: 'Hay vl', published: true }
+        },
+        users(parents, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+            return users.filter((user) => {
+                return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+            })
         }
     }
 }
