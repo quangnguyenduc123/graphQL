a3359c83908d7e95f980db77de78499c92458a65
quangnguyenduc123 <46807965+quangnguyenduc123@users.noreply.github.com>
2021-09-19 22:02:55 +0700

    custom type query

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type defnitions(schema)
 const typeDefs = `
     type Query{
-        hello: String!
+        user: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
         name: String!
+        age: Int!
+        email: String
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 // Resolvers
 const resolvers = {
     Query: {
-        hello() { return 'Hello world' },
-        name() { return 'My name is Quang' }
+        user() {
+            return { id: '1', name: 'quang', age: '25' }
+        },
+        post() {
+            return { id: '1', title: 'post1', body: 'Hay vl', published: true }
+        }
     }
 }
 
